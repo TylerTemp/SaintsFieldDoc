ed5f2a5a1ae3f853bd0dcd6aabd11453ad69ddf3
TylerTemp <tylertempdev@gmail.com>
2025-02-09 15:09:06 +0000

    fix tip

--- src/Components/WrapMarkdown/GithubQuote/Tip.tsx
+++ src/Components/WrapMarkdown/GithubQuote/Tip.tsx
@@ -1,13 +1,13 @@
 import { PropsWithChildren } from 'react';
 import Basic from './Basic';
-import AnnouncementTwoToneIcon from '@mui/icons-material/AnnouncementTwoTone';
+import LightbulbTwoToneIcon from '@mui/icons-material/LightbulbTwoTone';
 import useTheme from '@mui/material/styles/useTheme';
 
 export default ({children}: PropsWithChildren) => {
     const theme = useTheme();
     return <Basic
         color={theme.palette.success.main}
-        icon={<AnnouncementTwoToneIcon color="success" />}
+        icon={<LightbulbTwoToneIcon color="success" />}
         title="Tip"
     >
         {children}
